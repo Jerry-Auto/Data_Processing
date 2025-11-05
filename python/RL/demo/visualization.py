20132fa4aa48a16bb9c6945dc9255569fc37d0fd
zhangjinrui <1763287396@qq.com>
2025-11-05 18:23:12 +0800

    DQN算法结束

---
+++ python/RL/demo/visualization.py
@@ -0,0 +1,13 @@
+
+""" conda install -c conda-forge libstdcxx-ng """
+def agent_play(env,agent):
+    observation, info = env.reset()
+    done=False
+    for _ in range(100):
+        while not done:
+            action = agent.take_action(observation)
+            next_state, reward, done, truncated, _= env.step(action)
+            observation = next_state
+        if done or truncated:
+            observation, info = env.reset()
+    env.close()
